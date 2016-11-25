a28019710e0a7a514c2a1fbf3527dc43be94a3cc
Hydrocat <vdrbandeiras@gmail.com>
2016-11-25 16:18:23 -0200

    heavy_commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def main():
-    
+from graph_importer import *
 
-if __name__ == "__main__":
-    main() 
+graph_importer = Graph_importer("graphs/LDGraph50_12.txt")
+graph_list = list(graph_importer.graphs())
+print(len(graph_list))
+graph = graph_list[0]
+print(graph.sets[12])
