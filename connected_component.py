412d91e4ad769ece6be70f1d2325211549a62eaf
wagjub <danielcostavalerio@hotmail.com>
2016-11-28 19:00:53 -0200

    operações com conjuntos

--- connected_component.py
+++ connected_component.py
@@ -1,5 +1,32 @@
 from copy import deepcopy
-def connected(graph)
-    gray = {}
-    gray[0] = deepcopy(graph[0])
-    black = {}
+from graph_importer import *
+
+def connected(graph_list):
+    
+    for graph in graph_list:
+
+        graph.sets.pop(graph.n_label)
+        backup = set.union(*graph.sets.values()[0:])
+       # print backup
+
+        for k in graph.sets.keys():
+            r = graph.sets.pop(k)
+
+              #  print (len(graph.sets.values()))
+                #print graph.sets.values()
+             #   set.union(*graph.sets.values()[0:])
+            #set.union(*graph.sets.values()[0:])
+            if (len(graph.sets.values()) is not 0):
+                if (not r.issubset(set.union(*graph.sets.values()[0:]))):
+     #                   print "r {}".format(r)
+      #                  print "union {}".format(set.union(*graph.sets.values()[0:]))
+       #                 print "one time"
+                        graph.sets[k] = r
+            else:
+                print("pqp")
+                graph.sets[k] = r
+        if not backup.issubset(set.union(*graph.sets.values()[0:])):
+            print "foi eliminado"
+        else:
+            print len(graph.sets)
+            print set.union(*graph.sets.values()[0:])
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from graph_importer import *
+from connected_component import *
 
 graph_importer = Graph_importer("graphs/LDGraph50_12.txt")
 graph_list = list(graph_importer.graphs())
-print(len(graph_list))
-graph = graph_list[0]
-print(graph.sets[12])
+#print(len(graph_list))
+graph = graph_list[1]
+
+#print(set.intersection(*graph.sets.values()[8:10]))
+#print()
+#print(graph.sets)
+#print()
+
+connected(graph_list)
